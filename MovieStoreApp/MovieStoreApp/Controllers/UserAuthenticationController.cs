7b94a19d1740205dda76fed560e3aed843a70060
Readul Salehen <readul7214@gmail.com>
2024-02-28 16:33:19 +0600

    initial

---
+++ MovieStoreApp/MovieStoreApp/Controllers/UserAuthenticationController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieStoreApp.Models.DTO;
+using MovieStoreApp.Repositories.Abstract;
+
+namespace MovieStoreApp.Controllers
+{
+    public class UserAuthenticationController : Controller
+    {
+        private IUserAuthenticationService authService;
+        public UserAuthenticationController(IUserAuthenticationService authService)
+        {
+            this.authService = authService;
+        }
+        public async Task<IActionResult> Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginModel model)
+        {
+            if (!ModelState.IsValid)
+                return View(model);
+
+            var result = await authService.LoginAsync(model);
+            if (result.StatusCode == 1)
+                return RedirectToAction("Index", "Home");
+            else
+            {
+                TempData["msg"] = "Could not logged in..";
+                return RedirectToAction(nameof(Login));
+            }
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await authService.LogoutAsync();
+            return RedirectToAction(nameof(Login));
+        }
+    }
+}
---
+++ MovieStoreApp/MovieStoreApp/Repositories/Abstract/IUserAuthenticationService.cs
@@ -0,0 +1,11 @@
+﻿using MovieStoreApp.Models.DTO;
+
+namespace MovieStoreApp.Repositories.Abstract
+{
+    public interface IUserAuthenticationService
+    {
+        Task<Status> LoginAsync(LoginModel model);
+        Task LogoutAsync();
+        Task<Status> RegisterAsync(RegistrationModel model);
+    }
+}
